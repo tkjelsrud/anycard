e668878510203c5b8435baa5d9384d94f314ef4c
tkjelsrud <tkjelsrud@gmail.com>
2019-12-13 09:58:16 +0100

    Update chatserver.js

--- chatserver.js
+++ chatserver.js
@@ -2,7 +2,7 @@
 // Optional. You will see this name in eg. 'ps' or 'top' command
 process.title = 'node-chat';
 // Port where we'll run the websocket server
-var webSocketsServerPort = 80;
+var webSocketsServerPort = 8080;
 // websocket and http servers
 var webSocketServer = require('websocket').server;
 var http = require('http');
