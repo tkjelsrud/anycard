df34b64e712927489dddb9e4949819876dee741c
tkjelsrud <tkjelsrud@gmail.com>
2019-12-13 14:06:26 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,5 +3,6 @@ const app = express()
 const port = 8080
 
 app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.static('public'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
