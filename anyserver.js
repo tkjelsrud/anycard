e8774aa7d9c7583a8aaaea43e9631f0394b469ae
Thomas Kjelsrud <thomaskjelsrud@Thomass-MacBook-Pro.local>
2019-12-14 10:46:48 +0100

    1

---
+++ anyserver.js
@@ -0,0 +1,16 @@
+
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 8081;
+
+app.use(express.static(__dirname + '/public'));
+
+function onConnection(socket){
+  socket.on('message', (data) => socket.broadcast.emit('message', data));
+}
+
+io.on('connection', onConnection);
+
+http.listen(port, () => console.log('listening on port ' + port));
