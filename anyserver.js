2e34f3dcc2af34f3792c40243a43b5bd10d62caa
Thomas Kjelsrud <tkjelsrud@Thomass-MacBook-Air-2.local>
2020-05-05 09:47:37 +0200

    1

--- anyserver.js
+++ anyserver.js
@@ -4,11 +4,13 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 8081;
+var test = 1;
 
 app.use(express.static(__dirname + '/public'));
 
 function onConnection(socket){
-  console.log("Hi");
+  test++;
+  console.log("Hi" + test);
   socket.on('message', (data) => socket.broadcast.emit('message', data));
 }
 
